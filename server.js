77dff4394f3ead5be3d5938c56fe46ea8a8abe25
Glitch (chill-resisted-armchair) <none>
2020-05-08 09:20:26 +0000

    🐁🚨 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+
+var bodyParser = require("body-parser");
+
+const bookRouter = require("./router/books.route.js");
+const userRouter = require("./router/users.route.js");
+
+
+app.set("view engine", "pug");
+app.set("views", "./views");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/books", bookRouter);
+app.use("/users", userRouter);
+
+app.get('/',function(req,res){
+  res.render('index')
+})
+
+
+
+// listen for requests :)
+const listener = app.listen(process.env.PORT, () => {
+  console.log("Your app is listening on port " + listener.address().port);
+});
